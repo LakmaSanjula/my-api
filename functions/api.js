0362b37e816f2481e4e677cb00a84793f02d3d80
LakmaSanjula-97 <lakmagamage97@gmail.com>
2024-11-20 22:52:23 +0530

    changes

---
+++ functions/api.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const serverless = require('serverless-http');
+const app = express();
+const router = express.Router();
+
+app.get("/.netlify/functions/api/greet", (req, res) => {
+  const name = req.query.name || "Lakma";
+  res.json({
+    message: `Hello, ${name}!`,
+  });
+});
+
+app.use('/.netlify/functions/api', router);
+
+module.exports.handler = serverless(app);
